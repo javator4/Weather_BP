087d1696f853ae2acf5f416bc6faf62ba3bc74c1
Bartosz <bartoszpawlikowsky@gmail.com>
2020-08-17 18:21:14 +0200

    code cleanup

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -3,24 +3,33 @@ package pl.sda;
 import org.apache.log4j.Logger;
 import pl.sda.model.Weather;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.StringReader;
+import java.util.Scanner;
+
 public class App {
 
     private static Logger logger = Logger.getLogger(App.class);
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws FileNotFoundException {
         logger.info("URUCHOMEINIE APLIKACJI");
         logger.warn("WARNING");
         logger.debug("DEBUG");
         logger.error("ERROR");
 
+        String key = "";
+                File f = new File("key.txt");
+        Scanner sr = new Scanner(f);
+        if(sr.hasNextLine()) key = sr.nextLine();
         JSONDataFaster weatherService = new JSONDataFaster(
                 "http://api.weatherstack.com/current",
-                "0"
+                key
         );
 
         JSONDataOrg weatherService1 = new JSONDataOrg(
                 "http://api.weatherstack.com/current",
-                "0"
+                key
         );
 
         weatherService.getJSONData("torun");
@@ -28,7 +37,7 @@ public class App {
         Weather weather = weatherService.getWeather();
         Weather weather1 = weatherService1.getWeather();
 
-        //System.out.println(weather.toString());
+        System.out.println(weather.toString());
         System.out.println(weather1.toString());
     }
 }
--- src/main/java/pl/sda/WeatherForecast.java
+++
@@ -1,7 +0,0 @@
-package pl.sda;
-
-import pl.sda.model.Weather;
-
-public interface WeatherForecast {
-    Weather getWeather();
-}
